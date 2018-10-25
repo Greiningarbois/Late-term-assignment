96ec9e03dc762a26286307358217cac57aa1cd33
Thormundur <Thormundur15@gmail.com>
2018-10-25 10:37:59 +0000

    Start of server setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const app = require("./app");
-const port = process.env.PORT || 3000;
+const app = require("./src/app");
+const port = process.env.PORT || 8080;
+
+app.set("port", port);
+app.listen(port);
 
 console.log('Server listening on port ' + port);
-
-app.listen(port);
