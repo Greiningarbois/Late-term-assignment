c6874de253bdd4f1cf0afc4d1cc56bfd5ad1b864
sindri69 <sindrii17@ru.is>
2018-10-26 11:51:40 +0000

    getfield passes test with an empty board

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,12 +1,16 @@
 var board = [];
+var number;
 var TicTacToe = {
-
 
 	makeEmptyBoard : function() {
 	  for (var i = 0; i <= 8; i++) {
 	    board[i] = "";
 	  }
 		return board;
+	},
+
+	getField : function(number) {
+	  return board[number];
 	},
 
 }
--- src/TicTacToe.test.js
+++ src/TicTacToe.test.js
@@ -9,3 +9,8 @@ test("Testing make empty board", () => {
   const expected = ["", "", "", "", "", "", "", "", ""];
   expect(TicTacToe.makeEmptyBoard()).toEqual(expect.arrayContaining(expected));
 });
+
+test("Testing getField with an empty board", () => {
+    TicTacToe.makeEmptyBoard();
+    expect(TicTacToe.getField(5)).toBe("");
+  });
