bc3850107cd72662c5877ba25afbbbef3330e17e
Thormundur <Thormundur15@gmail.com>
2018-10-28 16:27:37 +0000

    Fix for index.html position

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
 	new CleanWebpackPlugin('./dist/main.js'),
 	new HtmlWebpackPlugin({
-		template: "./src/client/index.html"
+		template: "./dist/index.html"
     })
   ],
   devServer: {
