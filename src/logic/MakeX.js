cb645527df119d94151876bcaa22458f8bf3a6c6
Thormundur <Thormundur15@gmail.com>
2018-10-28 13:56:10 +0000

    Made so that finally 2 players can play, alot to go still :/

--- src/logic/MakeX.js
+++
@@ -1,7 +0,0 @@
-function MakeX(){
-	var ticBoard = document.getElementsByClassName('tacTic');
-	
-	return ticBoard;
-}
-
-module.exports = MakeX;
--- src/logic/listenPlz.js
+++ src/logic/listenPlz.js
@@ -1,23 +1,24 @@
-function listenPlz(ticBoard){
-	ticBoard[0].addEventListener("click", function() {AddSign(0, ticBoard);});
-	ticBoard[1].addEventListener("click", function() {AddSign(1, ticBoard);});
-	ticBoard[2].addEventListener("click", function() {AddSign(2, ticBoard);});
-	ticBoard[3].addEventListener("click", function() {AddSign(3, ticBoard);});
-	ticBoard[4].addEventListener("click", function() {AddSign(4, ticBoard);});
-	ticBoard[5].addEventListener("click", function() {AddSign(5, ticBoard);});
-	ticBoard[6].addEventListener("click", function() {AddSign(6, ticBoard);});
-	ticBoard[7].addEventListener("click", function() {AddSign(7, ticBoard);});
-	ticBoard[8].addEventListener("click", function() {AddSign(8, ticBoard);});
+function listenPlz(ticBoard, playerTurn){
+	ticBoard[0].addEventListener("click", function() {AddSign(0, ticBoard, playerTurn);});
+	ticBoard[1].addEventListener("click", function() {AddSign(1, ticBoard, playerTurn);});
+	ticBoard[2].addEventListener("click", function() {AddSign(2, ticBoard, playerTurn);});
+	ticBoard[3].addEventListener("click", function() {AddSign(3, ticBoard, playerTurn);});
+	ticBoard[4].addEventListener("click", function() {AddSign(4, ticBoard, playerTurn);});
+	ticBoard[5].addEventListener("click", function() {AddSign(5, ticBoard, playerTurn);});
+	ticBoard[6].addEventListener("click", function() {AddSign(6, ticBoard, playerTurn);});
+	ticBoard[7].addEventListener("click", function() {AddSign(7, ticBoard, playerTurn);});
+	ticBoard[8].addEventListener("click", function() {AddSign(8, ticBoard, playerTurn);});
 }
 
-function AddSign(i, ticBoard){
-	if(true){
+function AddSign(i, ticBoard, playerTurn){
+	if(playerTurn.innerHTML == "Player X turn"){
 		ticBoard[i].innerHTML = "X";
+		playerTurn.innerHTML = "Player O turn";
 	}
 	else {
  		ticBoard[i].innerHTML = "O";
+		playerTurn.innerHTML = "Player X turn";
 	}
-	
 }
 
 module.exports = listenPlz;
