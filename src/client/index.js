1636b7c7c707dd455d8ec7f63449ebf7fb37a714
Kári Gunnarsson <karig17@ru.is>
2018-10-29 01:21:38 +0000

    bla

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 const listenPlz = require('../logic/listenPlz');
 
 
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) { 
 	var ticBoard = document.getElementsByClassName('tacTic');
 	var playerTurn = document.getElementById('playerTurn');
 	listenPlz(ticBoard, playerTurn);
